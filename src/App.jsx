c2c002998a9b49944bef760f91cee253a8fc6b69
restradarojo <restradarojo@gmail.com>
2024-04-26 19:38:20 -0600

    beggining front development

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ToDoList } from './components/ToDoList'
+import { addToDo, getAllToDo } from './utils/HandleApi'
 
 function App() {
   
+  const [toDo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isUpadating, setIsUpdating] = useState(false)
 
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [] )
+
+  
   return (
-  <ToDoList/>
+    <div className='App'>
+      <div className='container'>
+        <h1>TODO LIST</h1>
+        <div className='top'>
+          <input type="text" placeholder='Agregar tarea'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          />
+          <div className='add' onClick={ () => addToDo(text, setText, setToDo) }>Add Task</div>
+        </div>
+        <div className='list'>
+          {toDo.map((item) => <ToDoList key = {item._id} text = {item.text}/>)}
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,6 +1,16 @@
+import { AiFillDelete } from "react-icons/ai"
+import { BiEdit } from "react-icons/bi"
 
-export const ToDoList = () => {
+export const ToDoList = ({text, updateMode, deleteToDo}) => {
   return (
-    <div>ToDoList</div>
+    <div className="todo">
+      <div className="text">
+        {text}
+      </div>
+      <div className="icons">
+        <BiEdit className="icon" onClick={updateMode} />
+        <AiFillDelete className="icon" onClick={deleteToDo} />
+      </div>
+    </div>
   )
 }
